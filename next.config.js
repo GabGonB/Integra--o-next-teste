7c29a75d74c8c5c45876e544b52f3a8ecb92836e
Gabriel <gabriel612goncalves@gmail.com>
2025-11-24 02:31:46 -0300

    Css

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Integra--o-next-teste',
+  assetPrefix: '/Integra--o-next-teste/',
 };
 
 module.exports = nextConfig;
